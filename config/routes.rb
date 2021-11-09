49cf83a0a90883ca73e875e8195c6b7b07843225
cedric duvillier <ced_duv@hotmail.com>
2021-11-09 14:27:58 +0100

    fin exo rails crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # all
+  get 'tasks/index', to: 'tasks#index'
+  # create
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # find
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # edit
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
